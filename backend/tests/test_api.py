071b3d80a9c69403f5cd34aa674da12756442710
lilitttaa <128687876+lilitttaa@users.noreply.github.com>
2024-05-13 00:05:13 +0800

    [test] Add test_moonshot_summerize

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -28,7 +28,87 @@ def test_Given_youtube_url_When_fetch_and_write_transcript_Then_return_transcrip
     
 @pytest.mark.current
 def test_langchain_transcript_loader():
-    url = 'https://www.youtube.com/watch?v=iMPWx1v7ioM'
+    url = 'https://www.youtube.com/watch?v=A2-n6z8fjMM&t=4s'
     youtube = YoutubeScrapy(url)
     transcript = youtube.get_transcript_by_langchain()
-    print("transcript:", transcript)
+    from langchain_text_splitters import CharacterTextSplitter
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=0
+    )
+    split_docs = text_splitter.split_documents(transcript)
+    print("split_docs:", split_docs)
+
+def test_moonshot_llm():
+    from langchain_community.llms.moonshot import Moonshot
+    import os
+    # Generate your api key from: https://platform.moonshot.cn/console/api-keys
+    # os.environ["MOONSHOT_API_KEY"] = "MOONSHOT_API_KEY"
+    llm = Moonshot()
+    # or use a specific model
+    # Available models: https://platform.moonshot.cn/docs
+    # llm = Moonshot(model="moonshot-v1-128k")
+    # Prompt the model
+    print(llm.invoke("What is the difference between panda and bear?"))
+    
+
+def test_moonshot_summerize():
+    from langchain.chains.summarize import load_summarize_chain
+    from langchain_community.document_loaders import WebBaseLoader
+    from langchain_community.llms.moonshot import Moonshot
+    from langchain_text_splitters import CharacterTextSplitter
+    from langchain.chains.llm import LLMChain
+    from langchain_core.prompts import PromptTemplate
+    from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+    from langchain.chains.combine_documents.map_reduce import MapReduceDocumentsChain
+    from langchain.chains.combine_documents.reduce import ReduceDocumentsChain
+    llm = Moonshot()
+    # Map
+    map_template = """The following is a set of documents
+    {docs}
+    Based on this list of docs, please identify the main themes 
+    Helpful Answer:"""
+    map_prompt = PromptTemplate.from_template(map_template)
+    map_chain = LLMChain(llm=llm, prompt=map_prompt)
+    
+    # Reduce
+    reduce_template = """The following is set of summaries:
+    {docs}
+    Take these and distill it into a final, consolidated summary of the main themes. 
+    Helpful Answer:"""
+    reduce_prompt = PromptTemplate.from_template(reduce_template)
+    # Run chain
+    reduce_chain = LLMChain(llm=llm, prompt=reduce_prompt)
+    combine_documents_chain = StuffDocumentsChain(
+        llm_chain=reduce_chain, document_variable_name="docs"
+    )
+    # Combines and iteratively reduces the mapped documents
+    reduce_documents_chain = ReduceDocumentsChain(
+        # This is final chain that is called.
+        combine_documents_chain=combine_documents_chain,
+        # If documents exceed context for `StuffDocumentsChain`
+        collapse_documents_chain=combine_documents_chain,
+        # The maximum number of tokens to group documents into.
+        token_max=8000,
+    )
+    
+    # Combining documents by mapping a chain over them, then combining results
+    map_reduce_chain = MapReduceDocumentsChain(
+        # Map chain
+        llm_chain=map_chain,
+        # Reduce chain
+        reduce_documents_chain=reduce_documents_chain,
+        # The variable name in the llm_chain to put the documents in
+        document_variable_name="docs",
+        # Return the results of the map steps in the output
+        return_intermediate_steps=False,
+    )
+
+    loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+    docs = loader.load()
+
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=0
+    )
+    split_docs = text_splitter.split_documents(docs)
+    # print("split_docs:", split_docs)
+    print(map_reduce_chain.run(split_docs))
